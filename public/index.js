368666782db1b7e1f6a1521513c200c963ff1176
vidhatashetty <toriumcar@gmail.com>
2020-07-28 12:45:00 +0530

    fix

--- public/index.js
+++ public/index.js
@@ -75,10 +75,14 @@ var textareaheightfunc = (scrollval) => {
     if(scrollval > 43){
         messageinput.style.height = `${h + (scrollval + 20 - 42)}px`;
         messages.style.bottom = messageinput.style.height;
+        var mh = window.innerHeight * 0.82;
+        messages.style.height = (mh - messageinput.style.height) + "px";
     }
     else{
         messageinput.style.height = h + "px";
         messages.style.bottom = messageinput.style.height;
+        var mh = window.innerHeight * 0.82;
+        messages.style.height = (mh - messageinput.style.height) + "px";
     }
 }
 
