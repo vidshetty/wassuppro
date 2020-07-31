52bc9aaa434880b64341a9f1b6b05707ce5c0188
vidhatashetty <toriumcar@gmail.com>
2020-07-31 10:58:22 +0530

    push trial8

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,7 @@ self.addEventListener('push',e => {
     var obj = e.data.json();
     console.log(obj);
     if(obj.type == "text"){
-        var title = obj.title;
+        var title = `${obj.title} (${obj.body.length} new messages)`;
         var completemsg = "";
         for(var i=0;i<obj.body.length;i++){
             completemsg += `${obj.body[i]}\n`;
@@ -11,6 +11,8 @@ self.addEventListener('push',e => {
             body: `${completemsg}`,
             icon: "./icons8-hangouts-512.png",
             badge: "./icons8-hangouts-96.png",
+            tag: "renotify",
+            renotify: true,
             data: {
                 message: "text"
             },
